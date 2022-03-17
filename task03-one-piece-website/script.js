925e65404cf6b125802e55e40f71c996afdab4d6
David Atias <datias@akurey.com>
2022-03-17 08:55:10 -0600

    Now the correct image is displayed according to size

--- task03-one-piece-website/script.js
+++ task03-one-piece-website/script.js
@@ -1,52 +1,52 @@
 "use strict"
 // hamburger menu
 
-const menuIcon = document.querySelector('.header__icon--menu');
-const xIcon = document.querySelector('.header__icon--x');
-const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector(".header__icon--menu");
+const xIcon = document.querySelector(".header__icon--x");
+const menu = document.querySelector(".menu");
 
-menuIcon.addEventListener('click', openMenu);
-xIcon.addEventListener('click', closeMenu)
+menuIcon.addEventListener("click", openMenu);
+xIcon.addEventListener("click", closeMenu)
 
 function openMenu() {
-  menuIcon.classList.add('hidden');
-  xIcon.classList.add('visible');
-  menu.classList.add('visible');
+  menuIcon.classList.add("hidden");
+  xIcon.classList.add("visible");
+  menu.classList.add("visible");
 }
 
 function closeMenu() {
   menuIcon.classList.remove("hidden");
-  xIcon.classList.remove('visible');
-  menu.classList.remove('visible');
+  xIcon.classList.remove("visible");
+  menu.classList.remove("visible");
 }
 
 // read all synopsis
 
-const btnSynopsis = document.querySelector('.synopsis__button');
-const synopsis = document.querySelector('.synopsis');
-const text = document.querySelectorAll('.synopsis__text--read-all');
-const logo = document.querySelector('.synopsis__logo');
-const opCharacters = document.querySelector('.op-characters');
+const btnSynopsis = document.querySelector(".synopsis__button");
+const synopsis = document.querySelector(".synopsis");
+const text = document.querySelectorAll(".synopsis__text--read-all");
+const logo = document.querySelector(".synopsis__logo");
+const opCharacters = document.querySelector(".op-characters");
 let btnClicked = true;
-btnSynopsis.addEventListener('click', clickSynopsis);
+btnSynopsis.addEventListener("click", clickSynopsis);
 
 function readAll() {
-  btnSynopsis.classList.add('clicked');
-  synopsis.classList.add('read-all');
-  logo.classList.add('visible');
-  opCharacters.classList.add('read-all');
+  btnSynopsis.classList.add("clicked");
+  synopsis.classList.add("read-all");
+  logo.classList.add("visible");
+  opCharacters.classList.add("read-all");
   for (let index = 0; index < text.length; index++) {
-    text[index].classList.add('visible');
+    text[index].classList.add("visible");
   }
 }
 
 function readLess() {
-  btnSynopsis.classList.remove('clicked');
-  synopsis.classList.remove('read-all');
-  logo.classList.remove('visible');
-  opCharacters.classList.remove('read-all');
+  btnSynopsis.classList.remove("clicked");
+  synopsis.classList.remove("read-all");
+  logo.classList.remove("visible");
+  opCharacters.classList.remove("read-all");
   for (let index = 0; index < text.length; index++) {
-    text[index].classList.remove('visible');
+    text[index].classList.remove("visible");
   }
 }
 
