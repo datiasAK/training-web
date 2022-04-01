456b7be9e97b920002a80b49bc17b86dde6c8469
David Atias <datias@akurey.com>
2022-04-01 08:22:53 -0600

    Added main component

--- task09-react/src/App.js
+++ task09-react/src/App.js
@@ -1,13 +1,13 @@
 import Footer from './components/Footer';
 import skull from './assets/img/op-skull.svg'
 import Header from './components/Header';
-import Synopsis from './components/Synopsis';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
       <Header />
-      <Synopsis/>
+      <Main/>
       <Footer 
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit at vel congue scelerisque tortor tortor."
         img={{src: skull, alt: "one piece skull"}} 
