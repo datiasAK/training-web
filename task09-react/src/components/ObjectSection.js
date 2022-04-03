6e5a43dac7b847c66b1917dcde506b1ec52211f6
David Atias <datias@akurey.com>
2022-04-02 21:25:30 -0600

    Repeated objects are now created in a loop

--- task09-react/src/components/ObjectSection.js
+++ task09-react/src/components/ObjectSection.js
@@ -3,31 +3,28 @@ import MysticObject from './MysticObject';
 import object from '../assets/img/object.svg'
 
 class ObjectSection extends Component {
-  render() { 
+  renderObjects(objectCount) {
     const objectName = "Gomu Gomu No Mi";
     const description = "Is a Paramecia-type Devil Fruit that gives the user's body the properties of rubber, making the user a Rubber Human."
-                      + "It was then accidentally eaten by the series protagonist, Monkey D. Luffy."
+                      + "It was then accidentally eaten by the series protagonist, Monkey D. Luffy.";
+    let objects = [];
+    for (let i = 0; i < objectCount; ++i) {
+      objects.push(<MysticObject key={i} name={objectName} description={description} img={object} />);
+    }
+    return objects;
+  }
+
+  render() { 
     return (
       <article className="article article--mist">
-      <div className="article__container article__container--mist">
-        <h3 className="article__title article__title--mist">Mist Objects</h3>
-        <a href="#" className="article__link article__link--mist">See all</a>
-      </div>
-      <div className="article__flex article__flex--mist">
-        <MysticObject name="Gomu Gomu No Mi" img={object} alt="Gomu Gomu No Mi">
-          {description}
-        </MysticObject>
-        <MysticObject name="Gomu Gomu No Mi" img={object} alt="Gomu Gomu No Mi">
-          {description}
-        </MysticObject>
-        <MysticObject name="Gomu Gomu No Mi" img={object} alt="Gomu Gomu No Mi">
-          {description}
-        </MysticObject>
-        <MysticObject name="Gomu Gomu No Mi" img={object} alt="Gomu Gomu No Mi">
-          {description}
-        </MysticObject>
-      </div>
-    </article>
+        <div className="article__container article__container--mist">
+          <h3 className="article__title article__title--mist">Mist Objects</h3>
+          <a href="#" className="article__link article__link--mist">See all</a>
+        </div>
+        <div className="article__flex article__flex--mist">
+          {this.renderObjects(4)}
+        </div>
+      </article>
     );
   }
 }
